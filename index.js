4ccf8f901da3909488688ca093fba8dd08398259
t-kalden <kalden.t@outlook.com>
2022-01-14 15:42:18 -0500

    Simple multipage site utilizing express, node.js, and bootstrap.

---
+++ index.js
@@ -0,0 +1,41 @@
+import express from 'express';
+import path from 'path';
+import redditData from './data.json';
+
+// const redditData = require('./data.json');
+console.log(redditData)
+const app = express();
+const __dirname = path.resolve();
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
+app.get("/", (req, res) => {
+    res.render('home');
+})
+
+app.get("/rand", (req, res) => {
+    const rand = Math.floor(Math.random() * 10) + 1;
+    res.render('random', { rand });
+})
+
+app.get('/r/:subreddit', (req, res) => {
+    const { subreddit } = req.params;
+    const data = redditData[subreddit];
+    if (data) {
+        res.render('subreddit', { ...data });
+    } else {
+        res.render('notfound', { subreddit });
+    }
+})
+
+app.get('/cats', (req, res) => {
+    const cats = [
+        'Blue', 'Rocket', 'Monty', 'Stephanie', 'Winston'
+    ]
+    res.render('cats', { cats });
+})
+
+app.listen(8080, () => {
+    console.log("Listening on port 3000.");
+})
